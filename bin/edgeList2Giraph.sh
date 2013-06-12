f9fb86ffcb9735677547ffbc343b0baeb8029174
Laurens Rietveld <laurens.rietveld@gmail.com>
2013-06-12 18:50:14 +0200

    added script to convert edge list to input suitable for giraph (yes, the input with the loooooong class name

---
+++ bin/edgeList2Giraph.sh
@@ -0,0 +1,117 @@
+#!/bin/bash
+#first some default values
+defaultNodeWeight=0
+defaultEdgeWeight=1
+
+
+
+
+nodeString=""
+nodeId=""
+function findNodeId {
+	nodeLine=`grep -P "$nodeString" $dictFile`;
+	if [ -z "$nodeLine" ]; then
+		nodeId=""
+	else
+		nodeId=`echo "$nodeLine" | awk '{print $2}'`;
+	fi
+}
+function addNodeToDict {
+	echo "$1	$nodeIterator" >> $dictFile;
+	nodeId=$nodeIterator
+	nodeIterator=$(($nodeIterator + 1))
+}  
+
+
+
+
+
+if [ -z "$1" ];then
+	echo "at least 1 argument required (edgelist input file, in tab separated format)"
+	echo "optional second arg for output file, and optional third arg for dictionary"
+	exit
+fi
+
+
+
+inputFile="$1"
+outputFile="${inputFile}_giraph"
+if [ -n "$2" ];then
+	outputFile="$2"
+fi
+dictFile="${inputFile}_dict"
+if [ -n "$3" ];then
+	dictFile="$3"
+fi
+#init empty files (overwrites as well!)
+> "$dictFile"
+> "$outputFile"
+
+
+echo "converting edgelist $inputFile into $outputFile"
+
+prevNode1String=""
+prevNode1Id=""
+lineNum=0;
+nodeIterator=0; #used for assigning IDs to nodes
+outgoingEdges=()
+sort $inputFile > "${inputFile}_sorted"
+while read line; do
+	lineNum=$(($lineNum + 1))
+	node1=`echo "$line" | awk '{print $1}'`;
+	node2=`echo "$line" | awk '{print $2}'`;
+	if [ -z "$node1" ] || [ -z "$node2" ] ; then
+		echo "WARN: unable to parse '$line' on line $lineNum. Skipping" 
+		continue
+	fi
+	if [ "$prevNode1String" == "$node1" ]; then
+		#find nodeId for outgoing edge
+		nodeString="$node2"
+		findNodeId;
+		if [ -z "$nodeId" ] ; then
+			addNodeToDict "$node2"
+		fi
+		outgoingEdges+=("$nodeId")
+	else 
+		if [ -n "$prevNode1String" ]; then
+			#first store the previous results (if there are any. on first run there arent)
+			edgesString="["
+			for i in "${outgoingEdges[@]}"; do
+			   edgesString+="[${i},${defaultEdgeWeight}],"
+			done
+			#need to remove final ','
+			edgesString="${edgesString:0:${#edgesString}-1}]"
+			echo "[${prevNode1Id},${defaultNodeWeight},${edgesString}]" >> "$outputFile"
+		fi
+			#[0,0,[[1,1],[3,3]]]
+		
+		#now reset values
+		prevNode1String="$node1"
+		
+		nodeString="$node1"
+		findNodeId;
+		if [ -z "$nodeId" ] ; then
+			addNodeToDict "$prevNode1String"
+		fi
+		prevNode1Id="$nodeId"
+		
+		outgoingEdges=()
+		nodeString="$node2"
+		findNodeId;
+		if [ -z "$nodeId" ] ; then
+			addNodeToDict "$node2"
+		fi
+		outgoingEdges+=("$nodeId")
+	fi
+done < sorted
+if [ -n "$prevNode1String" ]; then
+	#store last iteration
+	edgesString="["
+	for i in "${outgoingEdges[@]}"; do
+	   edgesString+="[${i},${defaultEdgeWeight}],"
+	done
+	#need to remove final ','
+	edgesString="${edgesString:0:${#edgesString}-1}]"
+	echo "[${prevNode1Id},${defaultNodeWeight},${edgesString}]" >> "$outputFile"
+		#[0,0,[[1,1],[3,3]]]
+fi
