28589bbc8802db9cfdf1360a949518897d3a69d5
Laurens Rietveld <laurens.rietveld@gmail.com>
2013-06-13 12:09:13 +0200

    made multi-project maven setup

---
+++ analysis/src/main/java/org/data2semantics/giraph/App.java
@@ -0,0 +1,13 @@
+package org.data2semantics.giraph;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
